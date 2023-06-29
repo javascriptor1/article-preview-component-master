587be1c1023b301d78aceb98b8a3e99ab5cc530a
Mohammed Fakih <mhd.fakih@gmail.com>
2023-06-29 13:13:08 +0300

    final solution for all parts of the challenge

--- main.js
+++ main.js
@@ -1,18 +1,41 @@
 "use strict";
 
-const shareArrow = document.querySelector("#share");
+// get arrows for mobile and desktop designs
+const shareArrowMobile = document.querySelector("#share-arrow-mobile");
+const shareArrowDesktop = document.querySelector("#share-arrow-desktop");
+
+// get Michelle profile to hide
+const meichellProfile = document.querySelector("#profile-name");
+
+// get divs for mobile and desktop active states to show
 const activeMobileState = document.querySelector("#share-social-media");
-const mechielProfile = document.querySelector('#profile-name');
-const shareActiveStateArrow = document.querySelector('#active-arrow');
+const activeDesktopState = document.querySelector(
+  "#share-social-media-desktop"
+);
 
+// get the div for desktop arrow share icon
+const arrowShareDesktopDiv = document.querySelector(
+  "#desktop-active-state-arrow-div"
+);
 
-shareArrow.addEventListener("click", function () {
-  mechielProfile.classList.toggle('hidden');
-  activeMobileState.classList.remove('hidden');
+// show social media div for mobile design
+shareArrowMobile.addEventListener("click", function () {
+  activeMobileState.classList.toggle("hidden");
 });
 
+// show social media div for desktop design + hide arrow shape and replace it with circled arrow
+shareArrowDesktop.addEventListener("click", () => {
+  showHide();
+});
 
-shareActiveStateArrow.addEventListener('click',function(){
-  mechielProfile.classList.toggle('hidden');
-  activeMobileState.classList.add('hidden');
-})
+// Hide social media div and exit active state for desktop design
+
+arrowShareDesktopDiv.addEventListener("click", () => {
+  showHide();
+});
+
+function showHide() {
+  activeDesktopState.classList.toggle("lg:flex");
+  arrowShareDesktopDiv.classList.toggle("hidden");
+  shareArrowDesktop.classList.toggle("lg:inline-block");
+}
